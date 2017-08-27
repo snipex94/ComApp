0dd367369dcab944002cc63a18f7988828d05c36
snipex94 <blaz.kvas@gmail.com>
2017-08-27 21:31:30 +0200

    Open/Close COM port. Writing data to log doesnt work

--- GemCom/Form1.Designer.cs
+++ GemCom/Form1.Designer.cs
@@ -29,11 +29,220 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.Button_Transmit_Data = new System.Windows.Forms.Button();
+            this.Button_Receive_Data = new System.Windows.Forms.Button();
+            this.ComboBox_Standard_Baudrates = new System.Windows.Forms.ComboBox();
+            this.ComboBox_Available_SerialPorts = new System.Windows.Forms.ComboBox();
+            this.Label_Available_Ports = new System.Windows.Forms.Label();
+            this.Label_Baudrate = new System.Windows.Forms.Label();
+            this.RichTextBox_Log = new System.Windows.Forms.RichTextBox();
+            this.GroupBox_Serial_Transmit = new System.Windows.Forms.GroupBox();
+            this.GroupBox_SeriaL_Receive = new System.Windows.Forms.GroupBox();
+            this.GroupBox_Serial_Settings = new System.Windows.Forms.GroupBox();
+            this.Button_Connect = new System.Windows.Forms.Button();
+            this.Button_Disconnect = new System.Windows.Forms.Button();
+            this.Label_COM_Status = new System.Windows.Forms.Label();
+            this.GroupBox_Commands = new System.Windows.Forms.GroupBox();
+            this.COMport = new System.IO.Ports.SerialPort(this.components);
+            this.GroupBox_Serial_Transmit.SuspendLayout();
+            this.GroupBox_SeriaL_Receive.SuspendLayout();
+            this.GroupBox_Serial_Settings.SuspendLayout();
+            this.GroupBox_Commands.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Button_Transmit_Data
+            // 
+            this.Button_Transmit_Data.Location = new System.Drawing.Point(43, 30);
+            this.Button_Transmit_Data.Name = "Button_Transmit_Data";
+            this.Button_Transmit_Data.Size = new System.Drawing.Size(105, 31);
+            this.Button_Transmit_Data.TabIndex = 0;
+            this.Button_Transmit_Data.Text = "Transmit";
+            this.Button_Transmit_Data.UseVisualStyleBackColor = true;
+            this.Button_Transmit_Data.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Button_Receive_Data
+            // 
+            this.Button_Receive_Data.Location = new System.Drawing.Point(56, 30);
+            this.Button_Receive_Data.Name = "Button_Receive_Data";
+            this.Button_Receive_Data.Size = new System.Drawing.Size(105, 30);
+            this.Button_Receive_Data.TabIndex = 1;
+            this.Button_Receive_Data.Text = "Receive";
+            this.Button_Receive_Data.UseVisualStyleBackColor = true;
+            // 
+            // ComboBox_Standard_Baudrates
+            // 
+            this.ComboBox_Standard_Baudrates.AutoCompleteCustomSource.AddRange(new string[] {
+            ""});
+            this.ComboBox_Standard_Baudrates.FormattingEnabled = true;
+            this.ComboBox_Standard_Baudrates.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400"});
+            this.ComboBox_Standard_Baudrates.Location = new System.Drawing.Point(83, 25);
+            this.ComboBox_Standard_Baudrates.Name = "ComboBox_Standard_Baudrates";
+            this.ComboBox_Standard_Baudrates.Size = new System.Drawing.Size(136, 21);
+            this.ComboBox_Standard_Baudrates.TabIndex = 2;
+            this.ComboBox_Standard_Baudrates.SelectionChangeCommitted += new System.EventHandler(this.ComboBox_Standard_Baudrates_SelectionChangeCommitted);
+            // 
+            // ComboBox_Available_SerialPorts
+            // 
+            this.ComboBox_Available_SerialPorts.FormattingEnabled = true;
+            this.ComboBox_Available_SerialPorts.Location = new System.Drawing.Point(336, 25);
+            this.ComboBox_Available_SerialPorts.Name = "ComboBox_Available_SerialPorts";
+            this.ComboBox_Available_SerialPorts.Size = new System.Drawing.Size(157, 21);
+            this.ComboBox_Available_SerialPorts.TabIndex = 4;
+            this.ComboBox_Available_SerialPorts.SelectionChangeCommitted += new System.EventHandler(this.ComboBox_Available_SerialPorts_SelectionChangeCommitted);
+            // 
+            // Label_Available_Ports
+            // 
+            this.Label_Available_Ports.AutoSize = true;
+            this.Label_Available_Ports.Location = new System.Drawing.Point(253, 28);
+            this.Label_Available_Ports.Name = "Label_Available_Ports";
+            this.Label_Available_Ports.Size = new System.Drawing.Size(77, 13);
+            this.Label_Available_Ports.TabIndex = 5;
+            this.Label_Available_Ports.Text = "Available Ports";
+            // 
+            // Label_Baudrate
+            // 
+            this.Label_Baudrate.AutoSize = true;
+            this.Label_Baudrate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Label_Baudrate.Location = new System.Drawing.Point(27, 28);
+            this.Label_Baudrate.Name = "Label_Baudrate";
+            this.Label_Baudrate.Size = new System.Drawing.Size(50, 13);
+            this.Label_Baudrate.TabIndex = 6;
+            this.Label_Baudrate.Text = "Baudrate";
+            // 
+            // RichTextBox_Log
+            // 
+            this.RichTextBox_Log.Location = new System.Drawing.Point(47, 265);
+            this.RichTextBox_Log.Name = "RichTextBox_Log";
+            this.RichTextBox_Log.Size = new System.Drawing.Size(540, 223);
+            this.RichTextBox_Log.TabIndex = 8;
+            this.RichTextBox_Log.Text = "";
+            // 
+            // GroupBox_Serial_Transmit
+            // 
+            this.GroupBox_Serial_Transmit.Controls.Add(this.Button_Transmit_Data);
+            this.GroupBox_Serial_Transmit.Location = new System.Drawing.Point(47, 83);
+            this.GroupBox_Serial_Transmit.Name = "GroupBox_Serial_Transmit";
+            this.GroupBox_Serial_Transmit.Size = new System.Drawing.Size(188, 176);
+            this.GroupBox_Serial_Transmit.TabIndex = 9;
+            this.GroupBox_Serial_Transmit.TabStop = false;
+            this.GroupBox_Serial_Transmit.Text = "Serial Transmit";
+            this.GroupBox_Serial_Transmit.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // GroupBox_SeriaL_Receive
+            // 
+            this.GroupBox_SeriaL_Receive.Controls.Add(this.Button_Receive_Data);
+            this.GroupBox_SeriaL_Receive.Location = new System.Drawing.Point(383, 83);
+            this.GroupBox_SeriaL_Receive.Name = "GroupBox_SeriaL_Receive";
+            this.GroupBox_SeriaL_Receive.Size = new System.Drawing.Size(204, 176);
+            this.GroupBox_SeriaL_Receive.TabIndex = 10;
+            this.GroupBox_SeriaL_Receive.TabStop = false;
+            this.GroupBox_SeriaL_Receive.Text = "Serial Receive";
+            // 
+            // GroupBox_Serial_Settings
+            // 
+            this.GroupBox_Serial_Settings.Controls.Add(this.ComboBox_Available_SerialPorts);
+            this.GroupBox_Serial_Settings.Controls.Add(this.Label_Baudrate);
+            this.GroupBox_Serial_Settings.Controls.Add(this.ComboBox_Standard_Baudrates);
+            this.GroupBox_Serial_Settings.Controls.Add(this.Label_Available_Ports);
+            this.GroupBox_Serial_Settings.Location = new System.Drawing.Point(47, 13);
+            this.GroupBox_Serial_Settings.Name = "GroupBox_Serial_Settings";
+            this.GroupBox_Serial_Settings.Size = new System.Drawing.Size(540, 64);
+            this.GroupBox_Serial_Settings.TabIndex = 11;
+            this.GroupBox_Serial_Settings.TabStop = false;
+            this.GroupBox_Serial_Settings.Text = "Serial Settings";
+            // 
+            // Button_Connect
+            // 
+            this.Button_Connect.Location = new System.Drawing.Point(25, 77);
+            this.Button_Connect.Name = "Button_Connect";
+            this.Button_Connect.Size = new System.Drawing.Size(90, 27);
+            this.Button_Connect.TabIndex = 12;
+            this.Button_Connect.Text = "Connect";
+            this.Button_Connect.UseVisualStyleBackColor = true;
+            this.Button_Connect.Click += new System.EventHandler(this.Button_Connect_Click);
+            // 
+            // Button_Disconnect
+            // 
+            this.Button_Disconnect.Location = new System.Drawing.Point(25, 110);
+            this.Button_Disconnect.Name = "Button_Disconnect";
+            this.Button_Disconnect.Size = new System.Drawing.Size(90, 27);
+            this.Button_Disconnect.TabIndex = 13;
+            this.Button_Disconnect.Text = "Disconnect";
+            this.Button_Disconnect.UseVisualStyleBackColor = true;
+            this.Button_Disconnect.Click += new System.EventHandler(this.Button_Disconnect_Click);
+            // 
+            // Label_COM_Status
+            // 
+            this.Label_COM_Status.AutoSize = true;
+            this.Label_COM_Status.Location = new System.Drawing.Point(53, 45);
+            this.Label_COM_Status.Name = "Label_COM_Status";
+            this.Label_COM_Status.Size = new System.Drawing.Size(35, 13);
+            this.Label_COM_Status.TabIndex = 14;
+            this.Label_COM_Status.Text = "label1";
+            this.Label_COM_Status.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // GroupBox_Commands
+            // 
+            this.GroupBox_Commands.Controls.Add(this.Label_COM_Status);
+            this.GroupBox_Commands.Controls.Add(this.Button_Connect);
+            this.GroupBox_Commands.Controls.Add(this.Button_Disconnect);
+            this.GroupBox_Commands.Location = new System.Drawing.Point(242, 83);
+            this.GroupBox_Commands.Name = "GroupBox_Commands";
+            this.GroupBox_Commands.Size = new System.Drawing.Size(135, 176);
+            this.GroupBox_Commands.TabIndex = 15;
+            this.GroupBox_Commands.TabStop = false;
+            this.GroupBox_Commands.Text = "Commands";
+            // 
+            // COMport
+            // 
+            this.COMport.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.COMport_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 525);
+            this.Controls.Add(this.GroupBox_Commands);
+            this.Controls.Add(this.GroupBox_SeriaL_Receive);
+            this.Controls.Add(this.GroupBox_Serial_Transmit);
+            this.Controls.Add(this.RichTextBox_Log);
+            this.Controls.Add(this.GroupBox_Serial_Settings);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.GroupBox_Serial_Transmit.ResumeLayout(false);
+            this.GroupBox_SeriaL_Receive.ResumeLayout(false);
+            this.GroupBox_Serial_Settings.ResumeLayout(false);
+            this.GroupBox_Serial_Settings.PerformLayout();
+            this.GroupBox_Commands.ResumeLayout(false);
+            this.GroupBox_Commands.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Button_Transmit_Data;
+        private System.Windows.Forms.Button Button_Receive_Data;
+        private System.Windows.Forms.ComboBox ComboBox_Standard_Baudrates;
+        private System.Windows.Forms.ComboBox ComboBox_Available_SerialPorts;
+        private System.Windows.Forms.Label Label_Available_Ports;
+        private System.Windows.Forms.Label Label_Baudrate;
+        private System.Windows.Forms.RichTextBox RichTextBox_Log;
+        private System.Windows.Forms.GroupBox GroupBox_Serial_Transmit;
+        private System.Windows.Forms.GroupBox GroupBox_SeriaL_Receive;
+        private System.Windows.Forms.GroupBox GroupBox_Serial_Settings;
+        private System.Windows.Forms.Button Button_Connect;
+        private System.Windows.Forms.Button Button_Disconnect;
+        private System.Windows.Forms.Label Label_COM_Status;
+        private System.Windows.Forms.GroupBox GroupBox_Commands;
+        private System.IO.Ports.SerialPort COMport;
     }
 }
 
--- GemCom/Form1.cs
+++ GemCom/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,110 @@ namespace GemCom
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ComboBox_Available_SerialPorts.Items.AddRange(SerialPort.GetPortNames());
+
+            string SystemInformation;
+
+            SystemInformation = " Machine Name = " + System.Environment.MachineName;
+            SystemInformation = SystemInformation + Environment.NewLine + " OS Version = " + System.Environment.OSVersion;
+            SystemInformation = SystemInformation + Environment.NewLine + " Processor Cores = " + System.Environment.ProcessorCount;
+
+            RichTextBox_Log.Text = SystemInformation;
+
+            GroupBox_SeriaL_Receive.Enabled = false;
+            GroupBox_Serial_Transmit.Enabled = false;
+            ComboBox_Standard_Baudrates.Enabled = false;
+            GroupBox_Commands.Enabled = false;
+
+            Label_COM_Status.Text = "Closed";
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComboBox_Available_SerialPorts_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            RichTextBox_Log.Text = ComboBox_Available_SerialPorts.SelectedItem.ToString() + " Selected";
+            ComboBox_Standard_Baudrates.Enabled = true;
+        }
+
+        private void ComboBox_Standard_Baudrates_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            RichTextBox_Log.Text = RichTextBox_Log.Text + Environment.NewLine + ComboBox_Standard_Baudrates.SelectedItem.ToString() + " Selected";
+            GroupBox_Commands.Enabled = true;
+        }
+
+        private void Button_Connect_Click(object sender, EventArgs e)
+        {
+            string Port_Name = ComboBox_Available_SerialPorts.SelectedItem.ToString();
+            int Baud_Rate = Convert.ToInt32(ComboBox_Standard_Baudrates.SelectedItem);
+            string Data = "Neki";
+
+            COMport.BaudRate = Baud_Rate;
+            COMport.PortName = Port_Name;
+
+            try
+            {
+                COMport.Open();
+            }
+            catch (System.IO.IOException SerialException)
+            {
+                MessageBox.Show(SerialException.ToString());
+                COMport.Close();
+            }
+            catch (UnauthorizedAccessException SerialException)
+            {
+                MessageBox.Show(SerialException.ToString());
+                COMport.Close();
+            }
+            catch (InvalidOperationException SerialException)
+            {
+                MessageBox.Show(SerialException.ToString());
+                COMport.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Unknown ERROR when opening serial port" + COMport);
+                COMport.Close();
+            }
+
+            if(COMport.IsOpen == true)
+            {
+                Label_COM_Status.Text = "Open";
+                RichTextBox_Log.Text = RichTextBox_Log.Text + Environment.NewLine + Port_Name + " opened at " + ComboBox_Standard_Baudrates.SelectedItem.ToString() + " baudrate";
+                GroupBox_SeriaL_Receive.Enabled = true;
+                GroupBox_Serial_Transmit.Enabled = true;
+            }
+            else
+            {
+                COMport.Close();
+                RichTextBox_Log.Text = RichTextBox_Log.Text + Environment.NewLine + Port_Name + " failed to open";
+            }
+        }
+
+        private void Button_Disconnect_Click(object sender, EventArgs e)
+        {
+            COMport.Close();
+            RichTextBox_Log.Text = RichTextBox_Log.Text + Environment.NewLine + COMport.PortName + " closed";
+            GroupBox_SeriaL_Receive.Enabled = false;
+            GroupBox_Serial_Transmit.Enabled = false;
+        }
+
+        private void COMport_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string indata = COMport.ReadLine();
+            RichTextBox_Log.Text = RichTextBox_Log.Text + Environment.NewLine + indata;
         }
     }
 }
